421d24b7351f3190089f25baf488e98144aa17a3
Spark <Maciel Fidelis>
2022-04-13 13:32:09 -0300

    Fazendo as transformation dos campos de UnitPrice e InvoiceDate

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -7,9 +7,23 @@ if __name__ == "__main__":
 	sc = SparkContext()
 	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Online Retail]"))
 
+	schema_online_retail = StructType([
+		StructField("InvoiceNo",  	IntegerType(),  True),
+		StructField("StockCode", 	StringType(),  	True),
+		StructField("Description",  StringType(),   True),
+		StructField("Quantity",  	IntegerType(),  True),
+		StructField("InvoiceDate",  StringType(), 	True),
+		StructField("UnitPrice",   	StringType(), 	True),
+		StructField("CustomerID",  	IntegerType(),  True),
+		StructField("Country",  	StringType(),  	True)
+	])
+
 	df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_online_retail)
+		          .schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
-	print(df.show())
+
+	# Transformation UnitPrice e InvoiceDate
+	df = (df.withColumn('UnitPrice', F.regexp_replace(F.col('UnitPrice'), ',', '.').cast('float'))
+		   .withColumn('InvoiceDate', F.to_timestamp(F.col('InvoiceDate'), 'd/MM/yyyy H:m')))
